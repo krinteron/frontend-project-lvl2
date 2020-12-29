390c12cdb2307e68d1d3d5d815ec7de3b6deffb5
Levan <krinteron@gmail.com>
2020-12-29 13:27:32 +0300

    Step 2 argumentsInHelp

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,7 @@ const { program } = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
