1cf15c45e159ed5e80f3c8c2d06a5c5b9b76efcf
levan <levan@tvrain.ru>
2021-06-18 23:05:15 +0300

    lint fix2

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
-export const readFile = (file) => {
-  return fs.readFileSync(path.resolve(process.cwd(), './__tests__/__fixtures__', file), 'utf-8');
-};
+export const readFile = (file) => fs.readFileSync(path.resolve(process.cwd(), './__tests__/__fixtures__', file), 'utf-8');
 
 export const getExt = (file) => path.extname(file);
